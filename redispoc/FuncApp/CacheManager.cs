ddcfe0cbf73470f8761f8264879eed008b043d94
John Manaloto <manalotoj@users.noreply.github.com>
2020-11-18 12:03:39 -0700

    Update CacheManager.cs

--- redispoc/FuncApp/CacheManager.cs
+++ redispoc/FuncApp/CacheManager.cs
@@ -31,15 +31,6 @@ namespace FuncApp
 
             var cache = connection.GetDatabase();
             await cache.ExecuteAsync("FLUSHDB");
-            //string name = req.Query["name"];
-
-            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //name = name ?? data?.name;
-
-            //string responseMessage = string.IsNullOrEmpty(name)
-            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult("done");
         }
